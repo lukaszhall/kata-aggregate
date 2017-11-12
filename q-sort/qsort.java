1ba8bf4966b32da75f381d7325cd14653b05242b
lukaszhall <hall@alum.mit.edu>
2017-11-12 11:38:50 -0500

    Bug fixes

--- q-sort/qsort.java
+++ q-sort/qsort.java
@@ -3,20 +3,17 @@ import java.util.Arrays;
 public class qsort {
 
     public static void main(String[] args) {
-
-
         test1();
-
-
     }
-
 
     private static void test1() {
         int[] testArray1 = {4, 5, 8, 9, 0, 5, 9, 1};
-        printArr(testArray1, qsort(testArray1, 0, testArray1.length-1));
-
+        testArray(testArray1);
     }
 
+    private static void testArray(int[] arr) {
+        printArr(testArray1, qsort(testArray1, 0, testArray1.length-1));
+    }
 
     private static void printArr(int[] unsortedArr, int[] sortedArr) {
         if (!verifyArrayOrder(sortedArr)) {
@@ -24,7 +21,6 @@ public class qsort {
         }
         System.out.println("Unsorted: "+ Arrays.toString(unsortedArr) + " Sorted: "+Arrays.toString(sortedArr));
     }
-
 
     private static boolean verifyArrayOrder(int[] testArray) {
         if (testArray.length < 2) {
@@ -48,12 +44,12 @@ public class qsort {
             return array;
         }
 
-        System.out.println();
-        System.out.println("Qsort("+Arrays.toString(array)+","+left+","+right+")");
+        //System.out.println();
+        //System.out.println("Qsort("+Arrays.toString(array)+","+left+","+right+")");
         int pivot = array[right];
-        System.out.println("Pivot: "+pivot);
+        //System.out.println("Pivot: "+pivot);
         int index = partition(array, left, right, pivot);
-        System.out.println("Index: "+index);
+        //System.out.println("Index: "+index);
         qsort(array, left, index-1);
         qsort(array, index, right);
 
@@ -63,7 +59,7 @@ public class qsort {
     public static int partition(int[] array, int left, int right, int pivot) {
 
         while (left <= right) {
-            System.out.println("Left: "+left+ "  Right: "+right+"  Pivot:"+pivot+"  Array:"+Arrays.toString(array));
+            //System.out.println("Left: "+left+ "  Right: "+right+"  Pivot:"+pivot+"  Array:"+Arrays.toString(array));
             while (array[left] < pivot) {left++;}
             while (array[right] > pivot) {right--;}
 
@@ -73,7 +69,7 @@ public class qsort {
                 right--;
             }
         }
-        System.out.println("Partition: "+left);
+        //System.out.println("Partition: "+left);
         return left;
     }
 
